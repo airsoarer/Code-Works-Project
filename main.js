92038d611c2ff43f0c8110d2c509b09a7607a261
M. A. Williams <mwilliamsii233203@gmail.com>
2018-06-26 12:58:00 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -39,6 +39,20 @@
 
 function init(){
     firebase.initializeApp(config);
+    $(document).on("orientationchange", function(e){
+        switch(window.orientation){
+            case -90: case 90:
+            $(".project").removeClass("col");
+            $(".project").removeClass("s8");
+
+            $(".project").addClass("col");
+            $(".project").addClass("s12");
+
+            break
+            default:
+            
+        }
+    })
     $.getJSON("https://api.trello.com/1/boards/6PVLjz20/?key=ad18609bec500062f9944b092d1601de&token=8a16dca9b23c590fdd6819d5b3123a3656eec64fba10bafd5f84f9cc44369b48&cards=all", function(results){
         for(var i = 0; i < results.cards.length; i++){
             closed = results.cards[i].closed;
